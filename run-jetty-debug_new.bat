5a91f4e5b49dc70229a8a0fa0a0f17fc4e6b0e9c
harikesh189 <harikesh.iiita@gmail.com>
2016-09-12 15:22:58 +0530

    Update run-jetty-debug_new.bat

--- run-jetty-debug_new.bat
+++ run-jetty-debug_new.bat
@@ -1,2 +1,2 @@
-set MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8484,server=y,suspend=n -Xmx512m -XX:MaxPermSize=128m -Dfile=F:\personal\project\NewSpringProject\src\main\resources\ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4.0 -Dpackaging=jar
+set MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8484,server=y,suspend=n -Xmx512m -XX:MaxPermSize=128m -Dfile=C:\Users\harikesh.kumar\.m2\repository\com\oracle\ojdbc6\11.2.0.4\ojdbc6-11.2.0.4.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4.0 -Dpackaging=jar
 call mvn clean jetty:run %1
